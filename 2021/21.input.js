6a363e96966bc2994d7091ef398c8c24f0375fb3
Konrad Jahn <konrad.jahn.84@gmail.com>
2022-01-06 13:31:28 +0100

    Removes input, inspect from index

--- 2021/21.input.js
+++
@@ -1,6 +0,0 @@
-// =============================  Advent of Code  =============================
-// Input Day 21 of 2021
-// See https://adventofcode.com/2021/day/21/input
-
-module.exports.input = `Player 1 starting position: 3
-Player 2 starting position: 7`;
